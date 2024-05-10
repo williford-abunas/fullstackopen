7ab73494bcdf35e039a3fbea688c9ca08130b8b3
williford-abunas <abunaswilliford@gmail.com>
2024-05-10 21:16:08 +1200

    step 5 - course info

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,40 +1,42 @@
 /* eslint-disable react/prop-types */
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  }
 
-  const total = parts.reduce((acc, cur) => acc + cur.exercises, 0)
+  const total = course.parts.reduce((acc, cur) => acc + cur.exercises, 0)
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
+      <Content course={course} />
       <Total total={total} />
     </div>
   )
 }
 
 const Header = (props) => {
-  return <h1>{props.course}</h1>
+  return <h1>{props.course.name}</h1>
 }
 
 const Content = (props) => {
   return (
     <>
-      {props.parts.map((part, i) => {
+      {props.course.parts.map((part, i) => {
         return <Part key={i} title={part.name} number={part.exercises} />
       })}
     </>
