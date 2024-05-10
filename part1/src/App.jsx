11c9fc8d666a1c3ab4dd8fdb1ec1857cffb84023
williford-abunas <abunaswilliford@gmail.com>
2024-05-10 21:12:09 +1200

    step 4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,25 +1,27 @@
 /* eslint-disable react/prop-types */
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ]
 
-  const total = part1.exercises + part2.exercises + part3.exercises
+  const total = parts.reduce((acc, cur) => acc + cur.exercises, 0)
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
       <Total total={total} />
     </div>
   )
@@ -32,9 +34,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part title={props.part1.name} number={props.part1.exercises} />
-      <Part title={props.part2.name} number={props.part2.exercises} />
-      <Part title={props.part3.name} number={props.part3.exercises} />
+      {props.parts.map((part, i) => {
+        return <Part key={i} title={part.name} number={part.exercises} />
+      })}
     </>
   )
 }
