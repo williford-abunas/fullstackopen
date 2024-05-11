d5505c6e9faf582d98a77300a9c72e585c831d81
williford-abunas <abunaswilliford@gmail.com>
2024-05-12 00:55:20 +1200

    unicafe done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,77 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClick = (e) => {
+    if (e.target.textContent === 'good') {
+      setGood((prev) => prev + 1)
+    }
+
+    if (e.target.textContent === 'neutral') {
+      setNeutral((prev) => prev + 1)
+    }
+
+    if (e.target.textContent === 'bad') {
+      setBad((prev) => prev + 1)
+    }
+  }
+  console.log(good, neutral, bad)
 
   return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button text="good" handleClick={handleClick} />
+      <Button text="neutral" handleClick={handleClick} />
+      <Button text="bad" handleClick={handleClick} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>
+}
+
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
+  const total = good + neutral + bad
+  return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Statistics</h1>
+      {total === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <table>
+          <tbody>
+            {good ? <StatisticLine text="good" value={good} /> : ''}
+            {neutral ? <StatisticLine text="neutral" value={neutral} /> : ''}
+            {bad ? <StatisticLine text="bad" value={bad} /> : ''}
+            <StatisticLine text="total" value={total} />
+            <StatisticLine text="average" value={(good - bad) / total} />
+            <StatisticLine
+              text="positives"
+              value={(good / total) * 100 + ` %`}
+            />
+          </tbody>
+        </table>
+      )}
     </>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>
+        {`${props.text} `}
+        {props.value}
+      </td>
+    </tr>
   )
 }
 
