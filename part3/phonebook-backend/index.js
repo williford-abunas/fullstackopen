2e50d64ccbe97b1a49f90106377acf255f6fec4c
williford-abunas <abunaswilliford@gmail.com>
2024-06-19 19:31:48 +1200

    step 2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -16,6 +16,11 @@ app.get('/api/persons', (req, res) => {
   res.json(data)
 })
 
+app.get('/info', (req, res) => {
+  const date = new Date()
+  res.send(`<p>Phonebook has info for ${data.length} people</p><p>${date}</p>`)
+})
+
 // SERVER
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
