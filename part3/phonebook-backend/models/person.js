beed45e6dc9d6eb60458b5f40da0bbe95bcdcfa3
williford-abunas <abunaswilliford@gmail.com>
2024-07-21 23:42:34 +1200

    mongo custom validator

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -21,8 +21,20 @@ mongoose
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d{5,}/.test(v)
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
 })
 
 personSchema.set('toJSON', {
